07e5ffcda8d426320c1f2324844be39ba633967b
Agnieszka Kowalewska <ah.dzwonkowska@student.uw.edu.pl>
2023-06-03 13:44:52 +0200

    Added readme_text

--- browser/pages/README.py
+++ browser/pages/README.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.title("README")
+st.title("Manual")
+
+st.markdown('#### Pipeline description')
+
+st.markdown('#### Parameters description and tuning')
+
+
+st.markdown('#### Output description')
+
+st.markdown('#### Limitations')
+# Labels need to be aligned...
